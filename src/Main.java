743593c26515fb40b7011ca57457f92a31ba179d
TatianaZhiryakova <tatjana_tv@list.ru>
2021-10-21 00:08:14 +0300

    added project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int ballance = 50;
+        int amount = 1500;
+        int bonus;
+
+        if (amount >=1000){
+            bonus = amount / 100;
+        } else{
+            bonus = 0;
+        }
+        int total = ballance + amount + bonus;
+        System.out.println(total);
+        System.out.println(bonus);
+    }
+}
